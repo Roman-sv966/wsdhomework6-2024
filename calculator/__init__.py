9c66c4ccaed0b4fc096f8e83b67e6a0982b0f767
Roman-sv966 <Roman-sv966>
2024-11-03 12:13:35 +0530

    calculations and operations implemented

---
+++ calculator/__init__.py
@@ -0,0 +1,23 @@
+from calculator.operations import add, subtract, divide, multiply
+from calculator.calculation import Calculation
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        calculation = Calculation(add, a ,b)
+        return calculation.get_output()
+
+    @staticmethod
+    def subtract(a, b):
+        calculation = Calculation(subtract, a ,b)
+        return calculation.get_output()
+
+    @staticmethod
+    def multiply(a, b):
+        calculation = Calculation(multiply, a ,b)
+        return calculation.get_output()
+
+    @staticmethod
+    def divide(a, b):
+        calculation = Calculation(divide, a, b)
+        return calculation.get_output()
